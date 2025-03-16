6168b90d17581ef1e7b5b8ea5ce1f559506aca4f
Jeet <jeet.d.desai@gmail.com>
2025-03-16 14:55:58 +0530

    Dropdown completed

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ https://www.zoho.com/commerce/free-demo.html
 
 https://www.dummyticket.com/dummy-ticket-for-visa-application/
 
+https://www.wikipedia.org/
+
+https://www.google.com/
+
 ---
 
 ### Content :
@@ -21,3 +25,4 @@ https://www.dummyticket.com/dummy-ticket-for-visa-application/
 1. Locator : css selector and Xpath locator
 2. Assertion : Implicit and Explicit
 3. Radio button and Check box
+4. Dropdown
--- cypress/e2e/Dropdown.cy.js
+++ cypress/e2e/Dropdown.cy.js
@@ -1,5 +1,5 @@
 describe("Handle Dropdowns", () => {
-  it("Dropdown with select", () => {
+  it.skip("Dropdown with select", () => {
     cy.visit("https://www.zoho.com/commerce/free-demo.html");
     cy.get("#zcf_address_country")
       .select("India")
@@ -8,10 +8,29 @@ describe("Handle Dropdowns", () => {
   //Skip testcase
   //   it.skip("Skipcase", () => {});
 
-  it("Dropdown without select", () => {
+  it.skip("Dropdown without select", () => {
     cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
     cy.get("#select2-billing_country-container").click(); //click dropdown
     cy.get(".select2-search__field").type("Italy").type("{enter}"); //Type and enter selection
     cy.get("#select2-billing_country-container").should("have.text", "Italy");
+  });
+  it.skip("dropdown with static auto suggestion", () => {
+    cy.visit("https://www.wikipedia.org/");
+    cy.get("#searchInput").type("Mumbai");
+    cy.get(".suggestion-text").contains("Mumbai Metro").click();
+  });
+
+  it("dropdown with dynamic auto suggestion", () => {
+    cy.visit("https://www.google.com/");
+    cy.get("#APjFqb").type("cypress automation");
+    cy.wait(4000); //to wait 4 sec
+    cy.get("div.wM6W7d>span").should("have.length", 13); //count auto suggestion
+    cy.get("div.wM6W7d>span").each(($el, index, $list) => {
+      // $el- particular element , $list = contain total elements(Auto suggestion count)
+      if ($el.text() == "cypress automation jobs") {
+        cy.wrap($el).click(); // Click when text found
+      }
+    });
+    cy.get("div.wM6W7d>span").should("have.value", "cypress automation jobs");
   });
 });
